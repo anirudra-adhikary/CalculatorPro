ddbcd754ca305383a3b9436b13ae7b96251eb3db
anirudra-adhikary <anirudraadhikary123@gmail.com>
2023-10-26 02:22:13 +0530

    First Commit. App is fully working.

---
+++ app/src/main/java/com/rudra/calculatorpro/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.rudra.calculatorpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rudra.calculatorpro.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.ExpressionBuilder
+import kotlin.math.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn0.setOnClickListener {
+            binding.display.append("0")
+        }
+
+        binding.btn1.setOnClickListener {
+            binding.display.append("1")
+        }
+
+        binding.btn2.setOnClickListener {
+            binding.display.append("2")
+        }
+
+        binding.btn3.setOnClickListener {
+            binding.display.append("3")
+        }
+
+        binding.btn4.setOnClickListener {
+            binding.display.append("4")
+        }
+
+        binding.btn5.setOnClickListener {
+            binding.display.append("5")
+        }
+
+        binding.btn6.setOnClickListener {
+            binding.display.append("6")
+        }
+
+        binding.btn7.setOnClickListener {
+            binding.display.append("7")
+        }
+
+        binding.btn8.setOnClickListener {
+            binding.display.append("8")
+        }
+
+        binding.btn9.setOnClickListener {
+            binding.display.append("9")
+        }
+
+        binding.btnPlus.setOnClickListener {
+            binding.display.append("+")
+        }
+
+        binding.btnMin.setOnClickListener {
+            binding.display.append("-")
+        }
+
+        binding.btnMul.setOnClickListener {
+            binding.display.append("*")
+        }
+
+        binding.btnDiv.setOnClickListener {
+            binding.display.append("/")
+        }
+
+
+        binding.btnPoint.setOnClickListener {
+            binding.display.append(".")
+        }
+
+        binding.btnClear.setOnClickListener {
+            binding.display.text = ""
+            binding.result.text = ""
+        }
+
+        binding.calculate.setOnClickListener {
+            try {
+                val value = binding.display.text.toString()
+                val expression = ExpressionBuilder(value).build()
+                val result = expression.evaluate()
+
+                if (result.isInfinite() || result.isNaN()) {
+                    binding.result.text = "Division by zero or invalid calculation"
+                } else {
+                    binding.result.text = result.toString()
+                }
+            } catch (e: ArithmeticException) {
+                // Handle division by zero exception
+                binding.result.text = "Division by zero"
+            } catch (e: Exception) {
+                // Handle other exceptions that might occur during evaluation
+                binding.result.text = "Error: ${e.message}"
+            }
+        }
+
+
+    }
+
+}
